44449b9de29dd1d3b942308492e482765b08240e
Matthew Hirsch <mhirsch309@gmail.com>
2019-03-13 18:17:35 -0400

    Added LEDTestForm VS Project
    This project successfully tests the ability to send data to and receive data from the Bluetooth Arduino.

---
+++ LEDTestForm/LEDTestForm/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace LEDTestForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("a");
+            String incData = serialPort1.ReadLine();
+            MessageBox.Show(incData, "Test Response from BT");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("b");
+            String incData = serialPort1.ReadLine();
+            MessageBox.Show(incData, "Test Response from BT");
+        }
+    }
+}
