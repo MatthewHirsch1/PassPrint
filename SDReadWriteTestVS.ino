2087359b55bd5d0855b633d47b2949503a8c0a94
Matthew Hirsch <mhirsch309@gmail.com>
2019-03-16 13:19:38 -0400

    add, save, and delete creds all finally work!

--- SDReadWriteTestVS.ino
+++ SDReadWriteTestVS.ino
@@ -18,7 +18,7 @@ String testFile = "test.txt"; // Define test file name as a global String
 int numCreds = 0; // Number of credentials in the file
 
 void setup() {
-  Serial.begin(115200); // Open serial communications and wait for port to open:
+  Serial.begin(9600); // Open serial communications and wait for port to open:
   while (!Serial) { ; } // Wait for serial port to connect. Needed for native USB port only
 
   //Serial.print("Initializing SD card..."); // Provide feedback on initialization
@@ -35,19 +35,28 @@ void setup() {
   addOneCredential(testFile, "yahoo.com:bob123@yahoo.com:456:");
   addOneCredential(testFile, "outlook.com:bob123@outlook.com:321:");
 
-  Serial.println("Reading Contents:--------------------");
-  readContents(testFile);
+  //Serial.println("Reading Contents:--------------------");
+  //readContents(testFile);
 
   addOneCredential(testFile, "aol.com:bob123@aol.com:789:");
-  Serial.println("Reading Contents:--------------------");
-  readContents(testFile);
+  //Serial.println("Reading Contents:--------------------");
+  //readContents(testFile);
+  //Serial.println("NumCreds: " + String(numCreds));
   //retrieveOneCredential(testFile,"yahoo.com");
 
   removeOneCredential(testFile, "outlook.com","bob123@outlook.com","321");
-  
-  Serial.println("Reading Contents after removal:--------------------");
-  
-  readContents(testFile);
+  //Serial.println("NumCreds: " + String(numCreds));
+
+  //Serial.println("Reading Contents after removal:--------------------");
+  //readContents(testFile);
+//  Serial.println("NumCreds: " + String(numCreds));
+//
+//  removeOneCredential(testFile, "gmail.com", "bob123@gmail.com", "123");
+//
+//  Serial.println("Reading Contents after removal:--------------------");
+//  readContents(testFile);
+//  Serial.println("NumCreds: " + String(numCreds));
+
 }
 
 void loop() {
@@ -55,19 +64,31 @@ void loop() {
     String data = Serial.readStringUntil('%');
     data.trim();
     if (data == "Add Credential") {
-      addOneCredential("test.txt", "test.com:bob123@test.com:password:");
+      String newAccount = Serial.readStringUntil(':');
+      String newUname = Serial.readStringUntil(':');
+      String newPwd = Serial.readStringUntil(':');
+      addOneCredential(testFile, newAccount + ":" + newUname + ":" + newPwd + ":");
     }
-  
     if (data == "Retrieve Credentials") {
-      Serial.flush();
+      //Serial.flush();
       Serial.println(numCreds);
       readContents(testFile);
     }
-    if (data == "Saving Credential") {
-      String account = Serial.readStringUntil(':');
-      String uname = Serial.readStringUntil(':');
-      String pwd = Serial.readStringUntil(':');
-      addOneCredential(testFile, account + ":" + uname + ":" + pwd + ":");
+    if (data == "Save Credential") {
+      String oldAccount = Serial.readStringUntil(':');
+      String oldUname = Serial.readStringUntil(':');
+      String oldPwd = Serial.readStringUntil(':');
+      removeOneCredential(testFile, oldAccount, oldUname, oldPwd);
+      String newAccount = Serial.readStringUntil(':');
+      String newUname = Serial.readStringUntil(':');
+      String newPwd = Serial.readStringUntil(':');
+      addOneCredential(testFile, newAccount + ":" + newUname + ":" + newPwd + ":");
+    }
+    if (data == "Delete Credential") {
+      String oldAccount = Serial.readStringUntil(':');
+      String oldUname = Serial.readStringUntil(':');
+      String oldPwd = Serial.readStringUntil(':');
+      removeOneCredential(testFile, oldAccount, oldUname, oldPwd);
     }
   
   
@@ -137,42 +158,60 @@ void addOneCredential(String fileName, String cred) {
 
 // Removes one credential (website, username, and password) from the database
 void removeOneCredential(String fileName, String siteName, String username, String password) {
-  myFile = SD.open(fileName); // Open the file for reading
-  // need numcreds check
-  //Serial.println("numCreds: " + String(numCreds));
   int newNumCreds = numCreds - 1;
+  //Serial.println("newNumCreds: " + String(newNumCreds));
   String arrCreds[newNumCreds];
-  if (myFile) {
-    // Initialize Strings to hold website name, username, and password
-    String site;
-    String uname;
-    String pwd;
-    //String arrCreds[] = [];
-    int arrSize = newNumCreds;
-    //Serial.println("About to go through file");
-    while (myFile.available()) { // Find credential for desired website
-      site = myFile.readStringUntil(':');
-      site.trim();
-      uname = myFile.readStringUntil(':');
-      uname.trim();
-      pwd = myFile.readStringUntil(':');
-      pwd.trim();
-      if (site == siteName && uname == username && pwd == password) { // Skip credential to be removed
-        //Serial.println("Found the matching cred");
-        continue;
-      } else { // Record valid credentials
-        String cred = site + ":" + uname + ":" + pwd + ":";
-        //Serial.println("Valid cred: " + cred);
-        //Serial.println("Insert into index: " + String(newNumCreds - arrSize));
-        arrCreds[newNumCreds - arrSize] = cred;
-        arrSize--;
-        if (arrSize == 0) {break;}
+  if (newNumCreds != 0) {
+    myFile = SD.open(fileName); // Open the file for reading
+    // need numcreds check
+    //Serial.println("numCreds: " + String(numCreds));
+    //int newNumCreds = numCreds - 1;
+    //Serial.println("newNumCreds: " + String(newNumCreds));
+    //String arrCreds[newNumCreds];
+    if (myFile) {
+      // Initialize Strings to hold website name, username, and password
+      String site;
+      String uname;
+      String pwd;
+      //String arrCreds[] = [];
+      int arrSize = newNumCreds;
+      //Serial.println("About to go through file");
+      while (myFile.available()) { // Find credential for desired website
+        site = myFile.readStringUntil(':');
+        site.trim();
+        uname = myFile.readStringUntil(':');
+        uname.trim();
+        pwd = myFile.readStringUntil(':');
+        pwd.trim();
+        if (site == siteName && uname == username && pwd == password) { // Skip credential to be removed
+          //Serial.println("Found the matching cred");
+          //if (Serial.read() == "\n") {Serial.println("Newline found");}
+          if (myFile.peek() != -1) { //Serial.println("There is more data to read"); 
+            continue;}
+          else {
+            //Serial.println("Reached end of file"); 
+            break;}
+        } else { // Record valid credentials
+          String cred = site + ":" + uname + ":" + pwd + ":";
+          //Serial.println("Valid cred: " + cred);
+          //Serial.println("Insert into index: " + String(newNumCreds - arrSize));
+          arrCreds[newNumCreds - arrSize] = cred;
+          arrSize--;
+          if (arrSize == 0) {break;}
+        }
       }
     }
-  }
+    myFile.close(); 
+    //Serial.println("File closed");
+  } // end of if newNumCreds != 0  
   // Remove old file and rewrite all valid credentials into new file
-  myFile.close();
+  //myFile.close(); 
+  //Serial.println("File closed");
   SD.remove(fileName);
+  //Serial.println("File removed");
+  myFile = SD.open(fileName, FILE_WRITE);
+  //Serial.println("File open for writing");
+  myFile.close();
   numCreds = 0;
   for (int i = 0; i < newNumCreds; i++) {
     addOneCredential(fileName, arrCreds[i]);
